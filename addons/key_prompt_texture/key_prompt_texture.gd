e57f6fc645bb0d4a49e3729b4bc39c14c701c994
TechnoPorg <jonah.janzen@gmail.com>
2023-09-27 19:55:44 -0600

    Optimize texture loading in KeyPromptTexture
    Now only the textures for the current controller will be in memory at any given time, as opposed to the previous system of loading them all upon initialization.
    This should reduce memory usage and startup times significantly, at the cost of a slight increase in loading demand when the controller changes.
    Also fixed a super minor typographigcal error in the code comments.

--- addons/key_prompt_texture/key_prompt_texture.gd
+++ addons/key_prompt_texture/key_prompt_texture.gd
@@ -22,6 +22,13 @@ extends Texture2D
 #region Static Members
 # All for internal use only
 
+# Because Godot has no static signals,
+# this little helper class and a static variable are used in place.
+class _StaticNotifier:
+	signal changed
+	
+static var _static_notifier : _StaticNotifier = _StaticNotifier.new()
+
 # The different types of controller handled in the current project.
 # These correspond to the unique filenames which will be loaded from JOY_BUTTON_* folders.
 static var _controller_names : Array
@@ -33,7 +40,13 @@ static var _controller_name_mappings : Dictionary = {}
 # Either "Default" for no or no supported controller,
 # or the type of controller currently connected.
 # Accessed by instances to determine which texture to display, since it is shared across the project.
-static var _current_controller_name : String = "Default"
+static var _current_controller_name : String = "Default":
+	get:
+		return _current_controller_name
+	set(value):
+		_current_controller_name = value
+		# Inform all instances to reload.
+		_static_notifier.changed.emit()
 
 # Setup for shared class data / actions
 static func _static_init():
@@ -59,7 +72,7 @@ static func _joy_connection_changed(device : int, connected : bool):
 		
 	var main_joy_name = Input.get_joy_name(0)
 	for alias in _controller_name_mappings:
-		# alias is a RegEx option here
+		# alias is a RegEx object here
 		if alias.search(main_joy_name):
 			_current_controller_name = _controller_name_mappings[alias]
 			return
@@ -77,20 +90,25 @@ var button : JoyButton:
 		return button
 	set(value):
 		button = value
-		_textures = {} # Reset textures
-		# Folder names come from the JoyButton enum
-		for filename in DirAccess.get_files_at("res://addons/key_prompt_texture/textures/JOY_BUTTON_%d" % button):
-			if filename.get_extension() == "png": # Exclude .import files
-				_textures[filename.get_basename()] = load("res://addons/key_prompt_texture/textures/JOY_BUTTON_%d/%s" % [button, filename])
-		emit_changed()
+		_changed()
 
-# Internal storage for the textures which can be displayed for this physical button.
-# Worth considering: Should textures only be loaded when a controller of a type is connected?
-var _textures : Dictionary
+# Internal storage for the texture which represents the physical button on the current controller.
+var _active_texture : Texture2D = PlaceholderTexture2D.new()
 
 func _init():
 	button = JOY_BUTTON_A
-	Input.joy_connection_changed.connect(emit_changed.unbind(2)) # When the connected controller changes, the texture displayed will change.
+	_static_notifier.changed.connect(_changed)
+
+# Called whenever the current controller or selected button changes.
+func _changed():
+	# Folder names come from the JoyButton enum.
+	var path = "res://addons/key_prompt_texture/textures/JOY_BUTTON_%d/%s.png" % [button, _current_controller_name]
+	if FileAccess.file_exists(path):
+		_active_texture = load(path)
+	else:
+		# Assume that the dev doesn't want anything to be shown, since not even a catch-all is configured.
+		_active_texture = PlaceholderTexture2D.new()
+	emit_changed()
 #endregion
 
 #region Texture2D Overrides
@@ -99,34 +117,23 @@ func _init():
 # It's just a wrapper which forwards calls to the currently active texture.
 
 func _draw(to_canvas_item, pos, modulate, transpose):
-	if _textures.has(_current_controller_name) and _textures[_current_controller_name] != null:
-		_textures[_current_controller_name].draw(to_canvas_item, pos, modulate, transpose)
+	_active_texture.draw(to_canvas_item, pos, modulate, transpose)
 	
 func _draw_rect(to_canvas_item, rect, tile, modulate, transpose):
-	if _textures.has(_current_controller_name) and _textures[_current_controller_name] != null:
-		_textures[_current_controller_name].draw_rect(to_canvas_item, rect, tile, modulate, transpose)
+	_active_texture.draw_rect(to_canvas_item, rect, tile, modulate, transpose)
 	
 func _draw_rect_region(to_canvas_item, rect, src_rect, modulate, transpose, clip_uv):
-	if _textures.has(_current_controller_name) and _textures[_current_controller_name] != null:
-		_textures[_current_controller_name].draw_rect_region(to_canvas_item, rect, src_rect, modulate, transpose, clip_uv)
+	_active_texture.draw_rect_region(to_canvas_item, rect, src_rect, modulate, transpose, clip_uv)
 
 func _get_height() -> int:
-	if _textures.has(_current_controller_name) and _textures[_current_controller_name] != null:
-		return _textures[_current_controller_name].get_height()
-	return 0
+	return _active_texture.get_height()
 	
 func _get_width() -> int:
-	if _textures.has(_current_controller_name) and _textures[_current_controller_name] != null:
-		return _textures[_current_controller_name].get_width()
-	return 0
+	return _active_texture.get_width()
 	
 func _has_alpha() -> bool:
-	if _textures.has(_current_controller_name) and _textures[_current_controller_name] != null:
-		return _textures[_current_controller_name].has_alpha()
-	return false
+	return _active_texture.has_alpha()
 
 func _is_pixel_opaque(x, y) -> bool:
-	if _textures.has(_current_controller_name) and _textures[_current_controller_name] != null:
-		return _textures[_current_controller_name]._is_pixel_opaque(x, y)
-	return false
+	return _active_texture._is_pixel_opaque(x, y)
 #endregion
