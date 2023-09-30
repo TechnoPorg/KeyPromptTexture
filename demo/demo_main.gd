03d2366a915f61131b4fbd3d0fa80a8b8d5553b8
TechnoPorg <jonah.janzen@gmail.com>
2023-09-29 19:59:47 -0600

    Add working demo
    This has 3 basic buttons with the default prompt icons shipped in the plugin.
    It also demonstrates basic localization remapping.

--- demo/demo_main.gd
+++ demo/demo_main.gd
@@ -1,9 +1,31 @@
 extends Control
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
+const BUTTON_PRESSED_TEXT = "%s pressed!"
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _ready():
+	%Back.grab_focus()
+	%Back.pressed.connect(%Info.set_text.bind(BUTTON_PRESSED_TEXT % "Back"))
+	%Previous.pressed.connect(%Info.set_text.bind(BUTTON_PRESSED_TEXT % "Previous"))
+	%Select.pressed.connect(%Info.set_text.bind(BUTTON_PRESSED_TEXT % "Select"))
+
+func _input(event : InputEvent):
+	if event is InputEventJoypadButton and event.pressed:
+		match(event.button_index):
+			JOY_BUTTON_BACK:
+				%Back.grab_focus()
+				%Back.emit_signal(&"pressed")
+			JOY_BUTTON_LEFT_SHOULDER:
+				%Previous.grab_focus()
+				%Previous.emit_signal(&"pressed")
+			JOY_BUTTON_A:
+				%Select.grab_focus()
+				%Select.emit_signal(&"pressed")
+				
+func change_locale(idx : int):
+	match(idx):
+		0:
+			TranslationServer.set_locale("en")
+		1:
+			TranslationServer.set_locale("de")
+		2:
+			TranslationServer.set_locale("zh")
